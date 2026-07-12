764308a95f289cdcda1eafa83c15a28d4cae8483
Andrew Kaminer <akaminer@gatech.edu>
2026-07-11 22:30:53 -0700

    Starting benchmarking

---
+++ cpp/benchmarks/addition.cpp
@@ -0,0 +1,54 @@
+#include "separate_ints.hpp"
+#include "single_int.hpp"
+#include "scaled_single_int.hpp"
+
+#include <benchmark/benchmark.h>
+#include <cstdint>
+
+void BM_Separate(benchmark::State& state) {
+  separate_ints::fixed_point32_t a {100, 725};
+  separate_ints::fixed_point32_t b {532, 3802};
+  for (auto _ : state) {
+    benchmark::DoNotOptimize(a + b);
+  }
+}
+
+void BM_Merged(benchmark::State& state) {
+  merged_ints::fixed_point32_t a {100, 725};
+  merged_ints::fixed_point32_t b {532, 3802};
+  for (auto _ : state) {
+    benchmark::DoNotOptimize(a + b);
+  }
+}
+
+void BM_Scaled(benchmark::State& state) {
+  scaled_int::fixed_point32_t<4> a {100, 725};
+  scaled_int::fixed_point32_t<4> b {532, 3802};
+  for (auto _ : state) {
+    benchmark::DoNotOptimize(a + b);
+  }
+}
+
+void BM_Scaled_Promotion(benchmark::State& state) {
+  scaled_int::fixed_point32_t<3> a {100, 725};
+  scaled_int::fixed_point32_t<4> b {532, 3802};
+  for (auto _ : state) {
+    benchmark::DoNotOptimize(b + a);
+  }
+}
+
+void BM_Integer(benchmark::State& state) {
+  uint32_t a {100725};
+  uint32_t b {5323802};
+  for (auto _ : state) {
+    benchmark::DoNotOptimize(a + b);
+  }
+}
+
+BENCHMARK(BM_Integer);
+BENCHMARK(BM_Separate);
+BENCHMARK(BM_Merged);
+BENCHMARK(BM_Scaled);
+BENCHMARK(BM_Scaled_Promotion);
+
+BENCHMARK_MAIN();
