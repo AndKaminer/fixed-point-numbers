b695fd5b4fefe9ed5cf7adc594a2920aca2eabad
Andrew Kaminer <andrewkaminer@gmail.com>
2026-07-20 22:21:41 -0700

    Generics

--- cpp/benchmarks/subtraction.cpp
+++ cpp/benchmarks/subtraction.cpp
@@ -1,78 +1,40 @@
+#include "benchmark/utils.h"
 #include "separate_ints.hpp"
 #include "single_int.hpp"
 #include "scaled_single_int.hpp"
 
+#include "fixed_point_util.hpp"
+
 #include <benchmark/benchmark.h>
 #include <cstdint>
 
-void BM_Separate(benchmark::State& state) {
-  separate_ints::fixed_point32_t a {100, 725};
-  separate_ints::fixed_point32_t b {532, 3802};
+template <typename T>
+void bench(benchmark::State& state) {
+  auto a {util::Make<T>::value(100, 725)};
+  auto b {util::Make<T>::value(532, 3802)};
   for (auto _ : state) {
-    benchmark::DoNotOptimize(a);
-    benchmark::DoNotOptimize(b);
-
-    auto c = a - b;
-
-    benchmark::DoNotOptimize(c);
-    ++a;
+    benchmark::DoNotOptimize(a = a - b);
   }
+}
+
+void BM_Separate(benchmark::State& state) {
+  bench<separate_ints::fixed_point32_t>(state);
 }
 
 void BM_Merged(benchmark::State& state) {
-  merged_ints::fixed_point32_t a {100, 725};
-  merged_ints::fixed_point32_t b {532, 3802};
-  for (auto _ : state) {
-    benchmark::DoNotOptimize(a);
-    benchmark::DoNotOptimize(b);
-
-    auto c = a - b;
-
-    benchmark::DoNotOptimize(c);
-    ++a;
-  }
+  bench<merged_ints::fixed_point32_t>(state);
 }
 
 void BM_Scaled(benchmark::State& state) {
-  scaled_int::fixed_point32_t<4> a {100, 725};
-  scaled_int::fixed_point32_t<4> b {532, 3802};
-  for (auto _ : state) {
-    benchmark::DoNotOptimize(a);
-    benchmark::DoNotOptimize(b);
-
-    auto c = a - b;
-
-    benchmark::DoNotOptimize(c);
-    ++a;
-  }
+  bench<scaled_int::fixed_point32_t<4>>(state);
 }
 
 void BM_Integer(benchmark::State& state) {
-  uint32_t a {100725};
-  uint32_t b {5323802};
-  for (auto _ : state) {
-    benchmark::DoNotOptimize(a);
-    benchmark::DoNotOptimize(b);
-
-    auto c = a - b;
-
-    benchmark::DoNotOptimize(c);
-    ++a;
-  }
+  bench<uint32_t>(state);
 }
 
 void BM_Float(benchmark::State& state) {
-  float a {100.725};
-  float b {532.3802};
-  for (auto _ : state) {
-    benchmark::DoNotOptimize(a);
-    benchmark::DoNotOptimize(b);
-
-    auto c = a - b;
-
-    benchmark::DoNotOptimize(c);
-    ++a;
-  }
+  bench<float>(state);
 }
 
 BENCHMARK(BM_Integer);
--- cpp/include/fixed_point_util.hpp
+++ cpp/include/fixed_point_util.hpp
@@ -1,6 +1,8 @@
 #include <concepts>
 #include <cstdint>
 
+namespace util {
+
 template <typename T>
 struct Make {
   constexpr static T value(uint16_t whole, uint16_t frac) {return T{whole, frac};}
@@ -20,8 +22,9 @@ struct Make<uint32_t> {
   constexpr static uint32_t value(uint16_t whole, uint16_t frac) {
     return whole * 10000 + frac * (frac < 10 ? 1000 : (frac < 100 ? 100 : (frac < 1000 ? 10 : 1)));
   }
-}
+};
 
+/**
 template <typename T>
 concept RelaxedNumericConcept = std::three_way_comparable<T> && requires(T obj) {
   ++obj;
@@ -29,4 +32,6 @@ concept RelaxedNumericConcept = std::three_way_comparable<T> && requires(T obj) 
   obj - obj;
   obj * obj;
   obj / obj;
+};
+**/
 };
